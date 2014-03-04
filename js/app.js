4b510930a5326964a17caa026a96864e3a12b07c
cosmicmule <manny@mannydasilva.com>
2014-03-04 07:38:52 -0800

    work completed version 001
    Work has been completed a set to review status

--- js/app.js
+++ js/app.js
@@ -24,3 +24,49 @@ if (valid) {
 else {
 	alert ("Oops, try again!");
 }
+
+$(document).ready(function(){
+  
+	/* carousel */
+	$('.bxslider').bxSlider({
+		infiniteLoop: false,
+		hideControlOnEnd: true
+	});
+
+	/* from submit */
+	$( "#contact_us_form" ).submit(function( e ) {
+  		e.preventDefault();
+
+  		var validate_formElements = $('.required');
+  		var obj = new Object;
+  		obj.validate_formElements = validate_formElements[0].value;
+  		
+  		if(validateEmail(obj.validate_formElements) != ""){
+  			alert("were good to go !!")
+  		}else{
+  			alert("Email input has errors, Please try again.")
+  		}
+
+	});
+
+	/* get xml data from server , append new data to list  */
+	$( "#what_others_are_saying_list" ).empty();
+	$.ajax({
+        type: "GET" ,
+        url: "what_other_are_saying_feed.xml" ,
+        dataType: "xml" ,
+        success: function(xml) {
+           $(xml).find('person').each(function(){
+		      var sTitle = $(this).find('message').text();
+		      var sPublisher = $(this).find('user').text();
+		      $("<li></li>").html(sTitle + "<br/><span>" + sPublisher + "</span>").appendTo("#what_others_are_saying_list");
+		   });
+        }
+    });  
+});
+
+
+function validateEmail(email) { 
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+} 
